b86b216ebf2d44b87321ee5abc090432e700fece
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:53:57 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:53:57 AM
+class Solution {
+    public int majorityElement(int[] nums) {
+     int cand=0;
+     int count=0;
+     for(int i=0;i<nums.length;i++){
+        if(count==0){
+            cand=nums[i];
+        }
+        if(cand==nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+     }
+     return cand;   
+    }
+}
