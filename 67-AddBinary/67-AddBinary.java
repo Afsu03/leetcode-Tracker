daf715d56e8241f8c7b9446b1470df09e99ad2a9
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:54:25 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:54:25 AM
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            
+            if (i >= 0) sum += a.charAt(i--) - '0';  // convert char to int
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            
+            result.append(sum % 2);   // append 0 or 1
+            carry = sum / 2;          // update carry
+        }
+
+        if (carry != 0) result.append(carry);
+        
+        return result.reverse().toString();
+    }
+}
