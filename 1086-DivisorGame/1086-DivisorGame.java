ceafc043018f79a29661eff22b64d54287281182
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:53:04 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:53:04 AM
+class Solution {
+    public boolean divisorGame(int n) {
+       if(n%2==0)
+       return true;
+       else 
+       return false;
+    }
+}
