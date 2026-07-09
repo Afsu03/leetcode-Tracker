2c78760aae13368bce19e407061f88263a7f0544
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:53:54 +0530

    Create 172-FactorialTrailingZeroes

---
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:53:54 AM
+class Solution {
+    public int trailingZeroes(int n) {
+      int count = 0;
+        while (n > 0) {
+            n /= 5;      
+            count += n;
+        }
+        return count;
+    }
+}
