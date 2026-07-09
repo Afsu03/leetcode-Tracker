ab6309a24972ae3ac7568b9c5ef8c03101340902
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:55:26 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -0,0 +1,39 @@
+// Last updated: 7/9/2026, 9:55:26 AM
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = true;
+
+        for (char c : s.toCharArray()) {
+
+            rows[currentRow].append(c);
+
+            if (currentRow == 0) {
+                goingDown = true;
+            } else if (currentRow == numRows - 1) {
+                goingDown = false;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
