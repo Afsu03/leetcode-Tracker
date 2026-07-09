501cb7f6e269d137cd2d60cb12e8f728565ca5d1
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:55:22 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:55:22 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int original=x;
+         if(x<0){
+            return false;
+        } 
+        while(x!=0){
+
+        int digit=x%10;
+        rev=(rev*10)+digit;
+        x=x/10;
+
+        }
+        if(original==rev){
+        return true;
+        }else{
+        return false;
+        }
+    }
+}
