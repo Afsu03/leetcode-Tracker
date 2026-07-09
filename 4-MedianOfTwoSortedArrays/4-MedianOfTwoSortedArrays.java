f44fcf4325b69e20532f6d8d0962cce993d3dca7
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:55:28 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:55:28 AM
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[]merged=new int[nums1.length+nums2.length];
+        int k=0;
+
+        for(int num:nums1){
+            merged[k++]=num;
+        }
+        for(int num:nums2){
+            merged[k++]=num;
+        }
+        java.util.Arrays.sort(merged);
+
+        int n=merged.length;
+
+        if(n%2==1){
+            return merged[n/2];
+        }else{
+            return (merged[n/2]+merged[n/2-1]) / 2.0;
+        
+    }
+
+    }
+}
