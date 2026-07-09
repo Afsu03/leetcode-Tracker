13829883912f8a5a6deb36bf30b5c5ee663f57b0
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:53:28 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:53:28 AM
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int index=0;
+        
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+        arr[index]=arr[i];
+        index++;
+        }
+    }
+    while(index < arr.length){
+        arr[index]=0;
+        index++;
+        }
+    }
+ }
