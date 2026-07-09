005813b71ec0ffc5e01228765d9e895b4c2b7618
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-09 09:53:05 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 9:53:05 AM
+class Solution {
+    public int fib(int n) {
+         if(n<=1)
+         return n;
+         return fib(n-1) + fib(n-2);
+         
+    }
+
+}
