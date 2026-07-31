4aa25fd467cf3e76b5465a36cb2582da99db6a0c
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-07-31 09:03:11 +0530

    Create 482-LicenseKeyFormatting

---
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -0,0 +1,24 @@
+// Last updated: 7/31/2026, 9:03:11 AM
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+
+        s = s.replace("-", "").toUpperCase();
+
+        StringBuilder ans = new StringBuilder();
+
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            ans.append(s.charAt(i));
+            count++;
+
+            if (count == k && i != 0) {
+                ans.append('-');
+                count = 0;
+            }
+        }
+
+        return ans.reverse().toString();
+    }
+}
