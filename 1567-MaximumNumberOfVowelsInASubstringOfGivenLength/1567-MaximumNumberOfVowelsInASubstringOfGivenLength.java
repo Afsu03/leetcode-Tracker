7c45111d16acc422927422face3fdfc92b9c3e25
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-09-01 13:58:50 +0530

    Create 1567-MaximumNumberOfVowelsInASubstringOfGivenLength

---
+++ 1567-MaximumNumberOfVowelsInASubstringOfGivenLength/1567-MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -0,0 +1,28 @@
+// Last updated: 9/1/2026, 1:58:49 PM
+class Solution {
+    public int maxVowels(String s, int k) {
+        int count = 0;
+        int max = 0;
+
+        for(int i=0;i<k;i++){
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+        }
+        max = count;
+
+        for(int i=k;i<s.length();i++){
+            if(isVowel(s.charAt(i - k))){
+                count--;
+            }
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+          max = Math.max(max, count);
+        }
+       return max;
+    }
+    public boolean isVowel(char c){
+        return c=='a'||c=='i'|| c=='o'||c=='u'||c=='e';
+    }
+}
