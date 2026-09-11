c78200d458ae64d032ecce3a3268dcee5bd911e6
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-09-11 14:13:08 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,9 @@
+// Last updated: 9/11/2026, 2:13:10 PM
+class Solution {
+    public boolean divisorGame(int n) {
+       if(n%2==0)
+       return true;
+       else 
+       return false;
+    }
+}
