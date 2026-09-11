042b5f8d48dc88abe43248c033ab61c056c635f1
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-09-11 14:13:19 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 9/11/2026, 2:13:20 PM
+class Solution {
+    public int fib(int n) {
+         if(n<=1)
+         return n;
+         return fib(n-1) + fib(n-2);
+         
+    }
+
+}
