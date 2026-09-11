7bba5745c7eb6e408650c1435e92cca44e329fc5
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-09-11 14:12:52 +0530

    Create 1137-NThTribonacciNumber

---
+++ 1137-NThTribonacciNumber/1137-NThTribonacciNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 9/11/2026, 2:12:54 PM
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0)
+        return 0;
+        if(n==1||n==2)
+        return 1;
+
+        int a=0,b=1,c=1;
+        for(int i=3;i<=n;i++){
+        int sum=a+b+c;
+        a=b;
+        b=c;
+        c=sum;
+        }
+        return c;   
+    }
+}
