65d26b391e2cf8eb71f65cfcf08f1d1ad4bc05fb
AFSANA KATHOON A <afsuanish03@gmail.com>
2026-09-11 14:11:25 +0530

    Create 1732-FindTheHighestAltitude

---
+++ 1732-FindTheHighestAltitude/1732-FindTheHighestAltitude.java
@@ -0,0 +1,16 @@
+// Last updated: 9/11/2026, 2:11:26 PM
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int alt = 0;
+        int maxalt = 0;
+
+        for(int i=0;i<gain.length;i++){
+            alt+=gain[i];
+
+            if(alt>maxalt){
+                maxalt = alt;
+            }
+        }
+        return maxalt;
+    }
+}
